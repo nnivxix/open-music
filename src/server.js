49883098e1ef75133756665f4a3fc910bc32ef79
nnivxix <adhe.hanasa@gmail.com>
2022-04-08 15:51:15 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -16,8 +16,16 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
-  // const SongsService
+  const authenticationsService = new AuthenticationsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -45,10 +53,19 @@ const init = async () => {
     {
       plugin: users,
       options: {
-        service: new UsersService(),
+        service: usersService,
         validator: UsersValidator,
       },
     },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    }
   ], );
 
   await server.start();
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -6,6 +6,7 @@ const {
 } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -45,6 +46,30 @@ class UsersService {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const {
+      id,
+      password: hashedPassword
+    } = result.rows[0];
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
+  }
+
+
 }
 
 module.exports = UsersService;
